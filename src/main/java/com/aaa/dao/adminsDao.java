1aa13e6d34a0ccb691e9bc7cab18b6796283eaba
Ljj <724455927@qq.com>
2020-08-24 09:46:57 +0800

    123

--- src/main/java/com/aaa/dao/adminsDao.java
+++ src/main/java/com/aaa/dao/adminsDao.java
@@ -3,7 +3,7 @@ package com.aaa.dao;
 import com.aaa.entity.admins;
 import tk.mybatis.mapper.common.Mapper;
 
-
+@org.apache.ibatis.annotations.Mapper
 public interface adminsDao extends Mapper<admins> {
 
 }
--- src/main/java/com/aaa/dao/applyDao.java
+++ src/main/java/com/aaa/dao/applyDao.java
@@ -5,6 +5,7 @@ import com.aaa.entity.apply;
 import tk.mybatis.mapper.common.Mapper;
 
 
+@org.apache.ibatis.annotations.Mapper
 public interface applyDao extends Mapper<apply> {
 
 }
