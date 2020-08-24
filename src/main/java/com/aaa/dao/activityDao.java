f46ed5f827c2f4d062b426277dbc24ea1b034564
Ljj <724455927@qq.com>
2020-08-24 09:45:08 +0800

    asdggh

--- src/main/java/com/aaa/dao/activityDao.java
+++ src/main/java/com/aaa/dao/activityDao.java
@@ -3,7 +3,7 @@ package com.aaa.dao;
 import com.aaa.entity.activity;
 import tk.mybatis.mapper.common.Mapper;
 
-@org.apache.ibatis.annotations.Mapper
+
 public interface activityDao extends Mapper<activity> {
 
 }
--- src/main/java/com/aaa/dao/applyDao.java
+++ src/main/java/com/aaa/dao/applyDao.java
@@ -3,7 +3,8 @@ package com.aaa.dao;
 import com.aaa.entity.apply;
 
 import tk.mybatis.mapper.common.Mapper;
-@org.apache.ibatis.annotations.Mapper
+
+
 public interface applyDao extends Mapper<apply> {
 
 }
