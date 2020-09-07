1621a717dbb9157b8c9915342cc128b0c7d8caea
吕佳冰 <2091608452@qq.com>
2020-09-07 08:36:02 +0800

    ppppppp

--- src/main/java/com/aaa/dao/Details_messageDao.java
+++ src/main/java/com/aaa/dao/Details_messageDao.java
@@ -15,6 +15,15 @@ public interface Details_messageDao extends tk.mybatis.mapper.common.Mapper<Deta
     List<Details_message> SelbyBmid(@Param("bmid") String bmid);
 
 
+    //count
+    @Select("select count(dmid) from details_message")
+    public int totalCount();
+
+
+    @Select("select dm.natives,dm.dmid,dm.truename,dm.Idcard,dm.pic,dm.constellation,dm.nation,dm.children,dm.education,dm.soliloquy,dm.monthly,bm.bmname from details_message dm left join basic_message bm on bm.bmid=dm.bmid")
+    List<Map<String,Object>> findAll();
+
+
 
     //前台首页根据id查询
     @Select("select bm.bmid,bm.bmname,dm.pic,bm.number from details_message dm left join basic_message bm on dm.dmid=bm.bmid where bm.bmid=#{bmid}")
--- src/main/java/com/aaa/dao/life_messageDao.java
+++ src/main/java/com/aaa/dao/life_messageDao.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @org.apache.ibatis.annotations.Mapper
 public interface life_messageDao extends Mapper<life_message> {
 
-    @Select("select count(tid) from teacher")
+    @Select("select count(lmid) from life_message")
     public int totalCount();
 
     @Select(" select lm.lmid,pt.ptname,lm.buyhouse,lm.buycar,lm.smoking,lm.drink,lm.city,lm.book,lm.hobby,bm.bmname  from life_message lm left join profession_type pt on pt.ptid=lm.ptid left join basic_message bm on bm.bmid=lm.bmid")
