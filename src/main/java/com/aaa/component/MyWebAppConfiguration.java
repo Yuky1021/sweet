f67be7b46e3ca14d2992c2d36a35e6084302211b
Administrator <https://github.com/BunnyUBW/demo401.git>
2020-09-07 14:58:40 +0800

    9/5 9:37

--- src/main/java/com/aaa/component/MyWebAppConfiguration.java
+++ src/main/java/com/aaa/component/MyWebAppConfiguration.java
@@ -2,9 +2,7 @@ package com.aaa.component;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class MyWebAppConfiguration extends WebMvcConfigurationSupport {
@@ -18,19 +16,34 @@ public class MyWebAppConfiguration extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/path/**").addResourceLocations("file:\\D:\\image\\");
         registry.addResourceHandler("/TV/**").addResourceLocations("file:\\D:\\classVido\\");
 
-        registry.addResourceHandler("/js/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\js\\");
-        registry.addResourceHandler("/js1/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\js1\\");
-        registry.addResourceHandler("/css/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\css\\");
-        registry.addResourceHandler("/css1/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\css1\\");
-        registry.addResourceHandler("/images/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\images\\");
-        registry.addResourceHandler("/images1/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\images1\\");
-        registry.addResourceHandler("/fonts/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\fonts\\");
-        registry.addResourceHandler("/fonts1/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\fonts1\\");
-        registry.addResourceHandler("/plugins/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\plugins\\");
-        registry.addResourceHandler("/styles/**").addResourceLocations("file:\\D:\\课堂\\三期项目\\sweet\\src\\main\\resources\\templates\\styles\\");
+        registry.addResourceHandler("/js/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\js\\");
+        registry.addResourceHandler("/js1/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\js1\\");
+        registry.addResourceHandler("/css/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\css\\");
+        registry.addResourceHandler("/css1/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\css1\\");
+        registry.addResourceHandler("/images/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\images\\");
+        registry.addResourceHandler("/images1/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\images1\\");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\fonts\\");
+        registry.addResourceHandler("/fonts1/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\fonts1\\");
+        registry.addResourceHandler("/plugins/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\plugins\\");
+        registry.addResourceHandler("/styles/**").addResourceLocations("file:\\D:\\S1\\sweet\\src\\main\\resources\\templates\\styles\\");
 
 
         super.addResourceHandlers(registry);
     }
 
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        // 注册拦截器
+        LoginInterceptor loginInterceptor = new LoginInterceptor();
+        InterceptorRegistration loginRegistry = registry.addInterceptor(loginInterceptor);
+        // 拦截路径
+        loginRegistry.addPathPatterns("/**/findAllById");
+        loginRegistry.addPathPatterns("/**/viptc");
+        loginRegistry.addPathPatterns("/**/GeRen");
+        loginRegistry.addPathPatterns("/**/SendXQ");
+        loginRegistry.addPathPatterns("/**/qlists");
+        loginRegistry.addPathPatterns("/**/mygz");
+        super.addInterceptors(registry);
+
+    }
 }
